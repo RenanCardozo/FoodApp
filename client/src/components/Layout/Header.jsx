0eebc1beb117ffcfecb692cf55ba0e024de3e447
RenanCardozo <ltrenan@hotmail.com>
2023-04-24 18:39:07 -0400

    first commit

---
+++ client/src/components/Layout/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import FoodImg from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Rachaels Food Truck</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={FoodImg} alt="A table full of food" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
+
+
+
+
